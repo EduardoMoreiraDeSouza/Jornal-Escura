44f207234e0334c042bb41156d8ccb647dba4d25
Eduardo Moreira (Moreira) <edmoreira043557@gmail.com>
2024-06-26 23:52:43 -0300

    Conteúdos

---
+++ _js/sbm.js
@@ -0,0 +1,56 @@
+/*SUBMENU Gêneros*/
+
+function sbm(lc){
+    document.getElementById('subm-gen-id').src=lc;
+}
+/*COMENTÁRIOS*/
+function exp1(url1){
+    document.getElementById('subm-home-id').src=url1;
+}
+function exp2(url2){
+    document.getElementById('subm-eventos-id').src=url2;
+}
+function exp3(url3){
+    document.getElementById('subm-edicoes-id').src=url3;
+}
+function exp4(url4){
+    document.getElementById('subm-fale-conosco-id').src=url4;
+}
+function exp5(url5){
+    document.getElementById('subm-sobre-c-id').src=url5;
+}
+function exp6(url6){
+    document.getElementById('subm-sobre-id').src=url6;
+}
+function altura(){
+    var alt = parseInt(document.getElementById('mt1-id').height);
+    valor = alt + 1250;
+    document.getElementById('mt1-id').height=valor;
+}
+/*IMG*/
+function inc1(img1){
+    document.getElementById('icones').src=img1;
+}
+function inc2(img2){
+    document.getElementById('icones').src=img2;
+}
+function inc3(img3){
+    document.getElementById('icones').src=img3;
+}
+function inc4(img4){
+    document.getElementById('icones').src=img4;
+}
+function inc5(img5){
+    document.getElementById('icones').src=img5;
+}
+function inc6(img6){
+    document.getElementById('icones').src=img6;
+}
+function inc7(img7){
+    document.getElementById('icones').src=img7;
+}
+
+/*IFRAME*/
+function iframe(posicao){
+    document.getElementById('mt1-id').src=posicao;
+}
